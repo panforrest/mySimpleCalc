19abc6806254437182b33325301091102a31cf8c
panforrest <guoqianp@gmail.com>
2018-03-21 22:09:27 -0400

    28:35 input field and key space background are ready with style

--- src/components/screens/Calc.js
+++ src/components/screens/Calc.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 
 class Calc extends Component {
     render(){
     	return(
-    	  <View>
-    		<Text>Test</Text>
+    	  <View style={{ flex: 1, backgroundColor: "red"}}>
+    		<TextInput style={{
+    			backgroundColor: "rgb(41, 41, 41)",
+    			height: 150, 
+                width: 100 + "%",
+                color: "rgb(255, 255, 255)",
+                fontSize: 48,
+                textAlign: "right"
+    		}}
+    		/>
           </View>
     	)
     }
