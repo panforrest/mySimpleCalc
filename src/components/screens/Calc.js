763e650a74ac63577fff3f279155e5b54fc2be58
panforrest <guoqianp@gmail.com>
2018-03-21 22:17:24 -0400

    31:30 create stylesheet to take care of styling

--- src/components/screens/Calc.js
+++ src/components/screens/Calc.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 
 class Calc extends Component {
     render(){
     	return(
     	  <View style={{ flex: 1, backgroundColor: "red"}}>
-    		<TextInput style={{
-    			backgroundColor: "rgb(41, 41, 41)",
-    			height: 150, 
-                width: 100 + "%",
-                color: "rgb(255, 255, 255)",
-                fontSize: 48,
-                textAlign: "right"
-    		}}
-    		/>
+    		<TextInput style={styles.input}/>
           </View>
     	)
     }
 }
+
+const styles = StyleSheet.create({
+	input: {
+		backgroundColor: "rgb(41, 41, 41)",
+		height: 150, 
+        width: 100 + "%",
+        color: "rgb(255, 255, 255)",
+        fontSize: 48,
+        textAlign: "right"
+	}
+})
 
 export default Calc
