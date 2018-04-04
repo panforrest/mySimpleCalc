aa0f90319ff76fd8bc45b34940851b9c2b0bff35
panforrest <guoqianp@gmail.com>
2018-04-04 01:53:58 -0400

    1:23:50 each individual buttons can be keyed in the display box, but not able to make string

--- src/components/screens/Calc.js
+++ src/components/screens/Calc.js
@@ -51,12 +51,13 @@ class Calc extends Component {
 
                 return (
                   <View style={styles.row}>
-                    <TouchableOpacity onPress={this.handleInput.bind(this, '0')}
+                    <TouchableOpacity onPress={this.handleInput.bind(this, this.validKeys[i])}
                       style={styles.button}>
                       
                       <Text style={styles.btnText}>{this.validKeys[i]}</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.button}>
+                    <TouchableOpacity onPress={this.handleInput.bind(this, this.validKeys[i+1])}
+                      style={styles.button}>
                       <Text style={styles.btnText}>{this.validKeys[i+1]}</Text>
                     </TouchableOpacity>
                   </View>
